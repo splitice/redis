1ddff94e115d00670ef793c3051931ee74fa32e5
splitice <mat999@gmail.com>
2014-06-24 21:17:57 +1000

    first attempt at dynamic number of parameters (keys)

--- src/redis.c
+++ src/redis.c
@@ -278,8 +278,8 @@ struct redisCommand redisCommandTable[] = {
     {"pfcount",pfcountCommand,-2,"w",0,NULL,1,1,1,0,0},
     {"pfmerge",pfmergeCommand,-2,"wm",0,NULL,1,-1,1,0,0},
     {"pfdebug",pfdebugCommand,-3,"w",0,NULL,0,0,0,0,0},
-	{"tahit", tahitCommand, 5, "wm", 0, NULL, 1,1,1, 0, 0},
-	{ "tacalc", tacalcCommand, 4, "r", 0, NULL, 1, 1, 1, 0, 0 }
+	{"tahit", tahitCommand, -5, "wm", 0, NULL,5,-1,1,0,0},
+	{ "tacalc", tacalcCommand, 4, "r", 0, NULL,4,4,1,0,0}
 };
 
 struct evictionPoolEntry *evictionPoolAlloc(void);
--- src/t_timeavg.c
+++ src/t_timeavg.c
@@ -24,74 +24,78 @@ robj *taTypeLookupWriteOrCreate(redisClient *c, robj *key) {
 }
 
 void tahitCommand(redisClient *c) {
-	robj *o = taTypeLookupWriteOrCreate(c, c->argv[1]);
 	long bucket_interval, by, ts;
 
 	//the bucket
-	if ((getLongFromObjectOrReply(c, c->argv[2], &bucket_interval, NULL) != REDIS_OK))
+	if ((getLongFromObjectOrReply(c, c->argv[1], &bucket_interval, NULL) != REDIS_OK))
 		return; 
 
-	if ((getLongFromObjectOrReply(c, c->argv[3], &by, NULL) != REDIS_OK))
-		return;
-
-	if ((getLongFromObjectOrReply(c, c->argv[4], &ts, NULL) != REDIS_OK))
-		return;
-
-	if (o == NULL || checkType(c, o, REDIS_TAVG)) {
-		return;
-	}
-
-	unsigned int bucketN = (ts / bucket_interval) % TA_BUCKETS;
-
-	time_average* ta = (time_average*)o->ptr;
-	ta->buckets[bucketN] += by;
-
-	//difference between the begining of the previously updated bucket and now.
-	//int limits the max time a value can be stale
-	int updated_ago = ((ts / bucket_interval) * bucket_interval) - ta->last_updated;
-
-	if (updated_ago > bucket_interval){
-		unsigned int clear_buckets = updated_ago / bucket_interval;
-
-		if (clear_buckets >= TA_BUCKETS){
-			clear_buckets = TA_BUCKETS - 1;
-		}
-
-		for (unsigned int i = 1; i < clear_buckets; i++){
-			unsigned int k = (bucketN - i) % TA_BUCKETS;
-			ta->buckets[k] = 0;
-		}
-
-		ta->buckets[bucketN] = 1;
-	}
-
-	ta->last_updated = (unsigned int)ts;
-
-	signalModifiedKey(c->db, c->argv[1]);
-	notifyKeyspaceEvent(REDIS_NOTIFY_LIST, "tahit", c->argv[1], c->db->id);
-	server.dirty++;
-
-	unsigned long sum = 0;
-	for (unsigned int i = 0; i < TA_BUCKETS; i++){
-		sum += ta->buckets[i];
-	}
-
-	addReplyLongLong(c, sum);
-}
-
-void tacalcCommand(redisClient *c){
-	robj *o;
-	if ((o = lookupKeyReadOrReply(c, c->argv[1], shared.nokeyerr)) == NULL || checkType(c, o, REDIS_TAVG)){
-		return;
-	}
-
-	long bucket_interval, ts;
-
-	if ((getLongFromObjectOrReply(c, c->argv[2], &bucket_interval, NULL) != REDIS_OK))
+	if ((getLongFromObjectOrReply(c, c->argv[2], &by, NULL) != REDIS_OK))
 		return;
 
 	if ((getLongFromObjectOrReply(c, c->argv[3], &ts, NULL) != REDIS_OK))
 		return;
+	
+	for (int i = 4; i < c->argc; i++){
+		robj *o = taTypeLookupWriteOrCreate(c, c->argv[i]);
+
+		if (o == NULL || checkType(c, o, REDIS_TAVG)) {
+			continue;
+		}
+
+		unsigned int bucketN = (ts / bucket_interval) % TA_BUCKETS;
+
+		time_average* ta = (time_average*)o->ptr;
+		ta->buckets[bucketN] += by;
+
+		//difference between the begining of the previously updated bucket and now.
+		//int limits the max time a value can be stale
+		int updated_ago = ((ts / bucket_interval) * bucket_interval) - ta->last_updated;
+
+		if (updated_ago > bucket_interval){
+			unsigned int clear_buckets = updated_ago / bucket_interval;
+
+			if (clear_buckets >= TA_BUCKETS){
+				clear_buckets = TA_BUCKETS - 1;
+			}
+
+			for (unsigned int i = 1; i < clear_buckets; i++){
+				unsigned int k = (bucketN - i) % TA_BUCKETS;
+				ta->buckets[k] = 0;
+			}
+
+			ta->buckets[bucketN] = 1;
+		}
+
+		ta->last_updated = (unsigned int)ts;
+
+		signalModifiedKey(c->db, c->argv[1]);
+		notifyKeyspaceEvent(REDIS_NOTIFY_LIST, "tahit", c->argv[1], c->db->id);
+		server.dirty++;
+
+		unsigned long sum = 0;
+		for (unsigned int i = 0; i < TA_BUCKETS; i++){
+			sum += ta->buckets[i];
+		}
+
+		addReplyLongLong(c, sum);
+	}
+}
+
+void tacalcCommand(redisClient *c){
+
+	long bucket_interval, ts;
+
+	if ((getLongFromObjectOrReply(c, c->argv[1], &bucket_interval, NULL) != REDIS_OK))
+		return;
+
+	if ((getLongFromObjectOrReply(c, c->argv[2], &ts, NULL) != REDIS_OK))
+		return;
+
+	robj *o;
+	if ((o = lookupKeyReadOrReply(c, c->argv[3], shared.nokeyerr)) == NULL || checkType(c, o, REDIS_TAVG)){
+		return;
+	}
 
 	time_average* ta = (time_average*)o->ptr;
 
