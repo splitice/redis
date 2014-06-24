e15a82362c8d3a0baadc9c471c17efd842e0f424
splitice <mat999@gmail.com>
2014-06-24 17:40:52 +1000

    argument count includes the command name, increase

--- src/redis.c
+++ src/redis.c
@@ -278,8 +278,8 @@ struct redisCommand redisCommandTable[] = {
     {"pfcount",pfcountCommand,-2,"w",0,NULL,1,1,1,0,0},
     {"pfmerge",pfmergeCommand,-2,"wm",0,NULL,1,-1,1,0,0},
     {"pfdebug",pfdebugCommand,-3,"w",0,NULL,0,0,0,0,0},
-	{"tahit", tahitCommand, 3, "wm", 0, NULL, 1,1,1, 0, 0},
-	{ "tacalc", tacalcCommand, 2, "r", 0, NULL, 1, 1, 1, 0, 0 }
+	{"tahit", tahitCommand, 5, "wm", 0, NULL, 1,1,1, 0, 0},
+	{ "tacalc", tacalcCommand, 3, "r", 0, NULL, 1, 1, 1, 0, 0 }
 };
 
 struct evictionPoolEntry *evictionPoolAlloc(void);
