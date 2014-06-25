203688bbda1af17ee0908b2603bbcec82a3f3d7f
splitice <mat999@gmail.com>
2014-06-25 21:59:35 +1000

    moved hyperloglog definitions to header

--- src/hyperloglog.h
+++ src/hyperloglog.h
@@ -3,6 +3,22 @@
 #ifndef __HYPERLOGLOG_H__
 #define __HYPERLOGLOG_H__
 
+/* The cached cardinality MSB is used to signal validity of the cached value. */
+#define HLL_INVALIDATE_CACHE(hdr) (hdr)->card[7] |= (1<<7)
+#define HLL_VALID_CACHE(hdr) (((hdr)->card[7] & (1<<7)) == 0)
+
+#define HLL_P 14 /* The greater is P, the smaller the error. */
+#define HLL_REGISTERS (1<<HLL_P) /* With P=14, 16384 registers. */
+#define HLL_P_MASK (HLL_REGISTERS-1) /* Mask to index register. */
+#define HLL_BITS 6 /* Enough to count up to 63 leading zeroes. */
+#define HLL_REGISTER_MAX ((1<<HLL_BITS)-1)
+#define HLL_HDR_SIZE sizeof(struct hllhdr)
+#define HLL_DENSE_SIZE (HLL_HDR_SIZE+((HLL_REGISTERS*HLL_BITS+7)/8))
+#define HLL_DENSE 0 /* Dense encoding. */
+#define HLL_SPARSE 1 /* Sparse encoding. */
+#define HLL_RAW 255 /* Only used internally, never exposed. */
+#define HLL_MAX_ENCODING 1
+
 struct hllhdr {
 	char magic[4];      /* "HYLL" */
 	uint8_t encoding;   /* HLL_DENSE or HLL_SPARSE. */
