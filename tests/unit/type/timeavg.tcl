e81ceedce772373cc4f459896ae911e8d9bad5ab
splitice <mat999@gmail.com>
2015-06-17 15:29:31 +1000

    add assertions to every command

--- tests/unit/type/timeavg.tcl
+++ tests/unit/type/timeavg.tcl
@@ -4,9 +4,9 @@ start_server {
     test {TAHIT - constant rate for one length} {
         # 3 each second, total of 60
 		for {set i 0} {$i < 20} {incr i} {
-			r tahit 1 1 [expr 100+$i] mytahit1
-			r tahit 1 1 [expr 100+$i] mytahit1
-			r tahit 1 1 [expr 100+$i] mytahit1
+			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
+			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
+			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
 		}
 		
         assert_equal 60 [r tacalc 1 1019 mytahit1]
@@ -16,13 +16,13 @@ start_server {
         # 4,2,4,2.... expected average of 3/s (60)
 		for {set i 0} {$i < 20} {incr i} {
 			if {fmod($i,2) == 0} {
-				r tahit 1 1 [expr 100+$i] mytahit2
-				r tahit 1 1 [expr 100+$i] mytahit2
-				r tahit 1 1 [expr 100+$i] mytahit2
-				r tahit 1 1 [expr 100+$i] mytahit2
+				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
 			}
-			r tahit 1 1 [expr 100+$i] mytahit2
-			r tahit 1 1 [expr 100+$i] mytahit2
+			assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+			assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
 		}
 		
         assert_equal 60 [r tacalc 1 1019 mytahit2]
