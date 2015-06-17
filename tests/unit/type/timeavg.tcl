edf786b4d4fe165160824f160cfd52e329d5924c
splitice <mat999@gmail.com>
2015-06-17 15:45:12 +1000

    switch to assert_equal for debugging purposes

--- tests/unit/type/timeavg.tcl
+++ tests/unit/type/timeavg.tcl
@@ -4,9 +4,9 @@ start_server {
     test {TAHIT - constant rate for one length} {
         # 3 each second, total of 60
 		for {set i 0} {$i < 20} {incr i} {
-			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
-			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
-			assert {[r tahit 1 1 [expr 100+$i] mytahit1] eq {OK}}
+			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
+			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
+			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
 		}
 		
         assert_equal 60 [r tacalc 1 1019 mytahit1]
@@ -16,13 +16,13 @@ start_server {
         # 4,2,4,2.... expected average of 3/s (60)
 		for {set i 0} {$i < 20} {incr i} {
 			if {fmod($i,2) == 0} {
-				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
-				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
-				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
-				assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
 			}
-			assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
-			assert {[r tahit 1 1 [expr 100+$i] mytahit2] eq {OK}}
+			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
 		}
 		
         assert_equal 60 [r tacalc 1 1019 mytahit2]
