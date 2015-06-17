2add588d7fd08167357a5ebc010c907ed3f4d733
splitice <mat999@gmail.com>
2015-06-17 15:53:13 +1000

    Attempt to fix mistakes in tests

--- tests/unit/type/timeavg.tcl
+++ tests/unit/type/timeavg.tcl
@@ -3,29 +3,31 @@ start_server {
 } {
     test {TAHIT - constant rate for one length} {
         # 3 each second, total of 60
+		set f 0
 		for {set i 0} {$i < 20} {incr i} {
-			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
-			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
-			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit1]
+			incr f
+			assert_equal $f [r tahit 1 1 [expr 100+$i] mytahit1]
+			incr f
+			assert_equal $f [r tahit 1 1 [expr 100+$i] mytahit1]
+			incr f
+			assert_equal $f [r tahit 1 1 [expr 100+$i] mytahit1]
 		}
 		
-        assert_equal 60 [r tacalc 1 1019 mytahit1]
+        assert_equal 60 [r tacalc 1 119 mytahit1]
     }
 	
 	test {TAHIT - varied rate for one length} {
         # 4,2,4,2.... expected average of 3/s (60)
 		for {set i 0} {$i < 20} {incr i} {
 			if {fmod($i,2) == 0} {
-				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
-				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
-				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
-				assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+				r tahit 1 1 [expr 100+$i] mytahit2
+				r tahit 1 1 [expr 100+$i] mytahit2
 			}
-			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
-			assert_equal OK [r tahit 1 1 [expr 100+$i] mytahit2]
+			r tahit 1 1 [expr 100+$i] mytahit2
+			r tahit 1 1 [expr 100+$i] mytahit2
 		}
 		
-        assert_equal 60 [r tacalc 1 1019 mytahit2]
+        assert_equal 60 [r tacalc 1 119 mytahit2]
     }
 	
 	test {TAHIT - constant rate for 1.5 length} {
