a9aad906db1e5aa35bad0317cd1172226b20aed9
Mathew Heard <splitice@users.noreply.github.com>
2017-02-06 08:53:48 +1100

    fix tahitx tests

--- tests/unit/type/timeavg.tcl
+++ tests/unit/type/timeavg.tcl
@@ -4,17 +4,27 @@ start_server {
 
     test {TAHITX - constant rate for one length} {
         # 3 each second, total of 60
-		set f 0
-		for {set i 0} {$i < 32} {incr i} {
-			incr f
-			assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
-			incr f
-			assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
-			incr f
-			assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
-		}
+	set f 0
+	for {set i 0} {$i < 32} {incr i} {
+		incr f
+		assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
+		incr f
+		assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
+		incr f
+		assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitx1]
+	}
 		
-        assert_equal 32 [r ttl mytahitx1]
+        assert_equal 96 [r ttl mytahitx1]
+    }
+    
+    test {TAHITX - 1 rate for one length} {
+	set f 0
+	for {set i 0} {$i < 32} {incr i} {
+		incr f
+		assert_equal $f [r tahitx 1 1 [expr 1024+$i] mytahitxa]
+	}
+		
+        assert_equal 32 [r ttl mytahitxa]
     }
 
     test {TAHIT - constant rate for one length} {
