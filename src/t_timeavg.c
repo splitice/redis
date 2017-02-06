a84418a4def77dda47be9a147c9e578a05b12607
splitice <mat999@gmail.com>
2017-02-06 13:59:18 +1100

    ...

--- src/t_timeavg.c
+++ src/t_timeavg.c
@@ -174,7 +174,7 @@ void _tahitCommand(redisClient *c, int expire) {
 		
 		//difference between the begining of the previously updated bucket and now.
 		//int limits the max time a value can be stale
-		bucketDiff = (((int)(bucketAbsolute  % 16777216)) - ta->time.last) % TA_BUCKETS;
+		bucketDiff = (bucketAbsolute % 16777216) - ta->time.last;
 
 		//If updated more than one bucket interval ago, we need to clear a bucket in between
 		if (ta->time.interval != bucket_interval){
